a61f1eaa176d985c8989c25f4ad136cfd438bbcb
Gilbert Ronaldo <gilbert@ronaldo.id>
2019-09-04 08:20:45 +0700

    clear model with logger save to ms_log_data

--- src/ModelWithLogger.php
+++ src/ModelWithLogger.php
@@ -18,38 +18,12 @@ class ModelWithLogger extends Model
     protected function performUpdate(Builder $query, array $options = [])
     {
         $result = parent::performUpdate($query, $options);
-        if($result){
-            // Additional insert log to table
-            $ref_data_id = $this->attributes[$this->primaryKey];
-            $data_type = $this->table;
-            $json =$this->attributes;
-
-            // Log::debug("ref_data_id: $ref_data_id");
-            // Log::debug("data_type: $data_type");
-            // Log::debug("json: ");
-            // Log::debug($json);
-
-            DB::statement("INSERT INTO ms_log_data (data_type, ref_data_id, last_data, created_at) VALUES ('$data_type', $ref_data_id, '".str_replace("'","",json_encode($json))."', now())");
-        }
         return true;
     }
 
     protected function performInsert(Builder $query, array $options = [])
     {
         $result = parent::performInsert($query, $options);
-        if($result){
-            // Additional insert log to table
-            $ref_data_id = $this->attributes[$this->primaryKey];
-            $data_type = $this->table;
-            $json = $this->attributes;
-
-            // Log::debug("ref_data_id: $ref_data_id");
-            // Log::debug("data_type: $data_type");
-            // Log::debug("json: ");
-            // Log::debug($json);
-
-            DB::statement("INSERT INTO ms_log_data (data_type, ref_data_id, last_data, created_at) VALUES ('$data_type', $ref_data_id, '".str_replace("'","",json_encode($json))."', now())");
-        }
         return true;
     }
 }
